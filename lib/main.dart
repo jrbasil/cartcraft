14db61d9dd930a85512a43e2f6134a5324fed001
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-04-12 07:10:34 -0400

    Adapt SurveyKit boilerplate inside of main

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:convert' show json;
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/services.dart' show rootBundle;
 import 'package:survey_kit/survey_kit.dart';
 
 void main() {
@@ -12,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'CartCraft',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,9 +28,9 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.black,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Code Dart'),
     );
   }
 }
@@ -45,72 +50,163 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _MyAppState createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _MyAppState extends State<MyApp> {
   @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  Widget build(BuildContext context) => MaterialApp(
+      home: Scaffold(
+        body: Container(
+          color: Colors.white,
+          child: Align(
+            alignment: Alignment.center,
+            child: FutureBuilder<Task>(
+              future: getSampleTask(),
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.done &&
+                    snapshot.hasData &&
+                    snapshot.data != null) {
+                  final task = snapshot.data!;
+                  return SurveyKit(
+                    onResult: (SurveyResult result) {
+                      if (kDebugMode) {
+                        print(result.finishReason);
+                      }
+                    },
+                    task: task,
+                    showProgress: true,
+                    localizations: const {
+                      'cancel': 'Cancel',
+                      'next': 'Next',
+                    },
+                    themeData: Theme.of(context).copyWith(
+                      colorScheme: ColorScheme.fromSwatch(
+                        primarySwatch: Colors.red,
+                      ).copyWith(
+                        onPrimary: Colors.white,
+                      ),
+                      primaryColor: Colors.red,
+                      backgroundColor: Colors.white,
+                      appBarTheme: const AppBarTheme(
+                        color: Colors.white,
+                        iconTheme: IconThemeData(
+                          color: Colors.red,
+                        ),
+                        titleTextStyle: TextStyle(
+                          color: Colors.red,
+                        ),
+                      ),
+                      iconTheme: const IconThemeData(
+                        color: Colors.red,
+                      ),
+                      textSelectionTheme: const TextSelectionThemeData(
+                        cursorColor: Colors.red,
+                        selectionColor: Colors.red,
+                        selectionHandleColor: Colors.red,
+                      ),
+                      cupertinoOverrideTheme: const CupertinoThemeData(
+                        primaryColor: Colors.red,
+                      ),
+                      outlinedButtonTheme: OutlinedButtonThemeData(
+                        style: ButtonStyle(
+                          minimumSize: MaterialStateProperty.all(
+                            const Size(150.0, 60.0),
+                          ),
+                          side: MaterialStateProperty.resolveWith(
+                                (Set<MaterialState> state) {
+                              if (state.contains(MaterialState.disabled)) {
+                                return const BorderSide(
+                                  color: Colors.grey,
+                                );
+                              }
+                              return const BorderSide(
+                                color: Colors.red,
+                              );
+                            },
+                          ),
+                          shape: MaterialStateProperty.all(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(8.0),
+                            ),
+                          ),
+                          textStyle: MaterialStateProperty.resolveWith(
+                                (Set<MaterialState> state) {
+                              if (state.contains(MaterialState.disabled)) {
+                                return Theme.of(context)
+                                    .textTheme
+                                    .button
+                                    ?.copyWith(
+                                  color: Colors.grey,
+                                );
+                              }
+                              return Theme.of(context)
+                                  .textTheme
+                                  .button
+                                  ?.copyWith(
+                                color: Colors.red,
+                              );
+                            },
+                          ),
+                        ),
+                      ),
+                      textButtonTheme: TextButtonThemeData(
+                        style: ButtonStyle(
+                          textStyle: MaterialStateProperty.all(
+                            Theme.of(context).textTheme.button?.copyWith(
+                              color: Colors.red,
+                            ),
+                          ),
+                        ),
+                      ),
+                      textTheme: const TextTheme(
+                        headline2: TextStyle(
+                          fontSize: 28.0,
+                          color: Colors.black,
+                        ),
+                        headline5: TextStyle(
+                          fontSize: 24.0,
+                          color: Colors.black,
+                        ),
+                        bodyText2: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                        ),
+                        subtitle1: TextStyle(
+                          fontSize: 18.0,
+                          color: Colors.black,
+                        ),
+                      ),
+                      inputDecorationTheme: const InputDecorationTheme(
+                        labelStyle: TextStyle(
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    surveyProgressbarConfiguration: SurveyProgressConfiguration(
+                      backgroundColor: Colors.white,
+                    ),
+                  );
+                }
+                return const CircularProgressIndicator.adaptive();
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+
+  Future<Task> getSampleTask() {
+    var task = NavigableTask(
+      id: TaskIdentifier(),
+      steps: [
+        InstructionStep(
+          title: 'Service Matcher',
+          text: 'Get service recommendations from Code Dart by submitting a brief form.\nAll responses are optional and your information is kept anonymous.',
+          buttonText: 'Start',
+        ),
+      ],
+    );
+    return Future.value(task);
   }
 }
