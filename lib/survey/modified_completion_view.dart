b6b81802a8ccab2ba7019bf6c695ececd49adb96
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-04-13 10:43:06 -0400

    Remove unused package imports

--- lib/survey/modified_completion_view.dart
+++ lib/survey/modified_completion_view.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
 import 'package:survey_kit/src/result/step/completion_step_result.dart';
-import 'package:survey_kit/src/steps/predefined_steps/completion_step.dart';
 import 'package:survey_kit/src/views/widget/step_view.dart';
 
 import 'modified_completion_step.dart';
