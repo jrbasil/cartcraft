b80d1baa72ec8be031fe3a9cae630a35ede262aa
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-04-13 11:33:22 -0400

    Update HomePage class

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'dart:js';
+
+import 'package:cartcraft/survey/survey_page.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
@@ -19,19 +22,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -40,6 +30,16 @@ class _HomePageState extends State<HomePage> {
     // The Flutter framework has been optimized to make rerunning build methods
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
+    void _navigateToSurvey() {
+      setState(() {
+        // This call to setState tells the Flutter framework that something has
+        // changed in this State, which causes it to rerun the build method below
+        // so that the display can reflect the updated values. If we changed
+        // _counter without calling setState(), then the build method would not be
+        // called again, and so nothing would appear to happen.
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => SurveyPage(title: "Survey")));
+      });
+    }
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -70,17 +70,18 @@ class _HomePageState extends State<HomePage> {
               'Some other description here:',
             ),
             Text(
-              '$_counter',
+              'Get recommendations or visit the store',
               style: Theme.of(context).textTheme.headline4,
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Action',
+        onPressed: _navigateToSurvey,
+        tooltip: 'Survey',
         child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
+      // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
