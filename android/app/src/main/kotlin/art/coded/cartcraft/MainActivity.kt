c78d006e677211faa5008b14809566bd232ae9b5
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-04-18 17:38:43 -0400

    Add WooSignal shop boiilerplate

--- android/app/src/main/kotlin/art/coded/cartcraft/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package art.coded.cartcraft
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,24 @@
-import 'package:flutter/cupertino.dart';
-import 'app.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/bootstrap/app.dart';
+import 'package:flutter_app/bootstrap/app_helper.dart';
+import 'package:flutter_app/bootstrap/boot.dart';
+import 'package:flutter_app/routes/router.dart';
+import 'package:nylo_framework/nylo_framework.dart';
 
-void main() async => runApp(const App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Nylo nylo = await Nylo.init(router: appRouter(), setup: boot);
+
+  String initialRoute = AppHelper.instance.appConfig.appStatus != null
+      ? '/home'
+      : '/no-connection';
+
+  runApp(
+    AppBuild(
+      navigatorKey: nylo.router.navigatorKey,
+      onGenerateRoute: nylo.router.generator(),
+      initialRoute: initialRoute,
+      debugShowCheckedModeBanner: false,
+    ),
+  );
+}
