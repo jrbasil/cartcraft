e82fcec0cdc036cc7f0df0eb4229c51b3a08f499
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-04-19 15:22:52 -0400

    Restore survey and result page files to project, removing null-safety features until Flutter minimum SDK is raised

--- lib/bootstrap/helpers.dart
+++ lib/bootstrap/helpers.dart
@@ -37,9 +37,81 @@ import 'package:nylo_framework/nylo_framework.dart';
 import 'package:platform_alert_dialog/platform_alert_dialog.dart';
 import 'package:pull_to_refresh/pull_to_refresh.dart';
 import 'package:status_alert/status_alert.dart';
+import 'package:survey_kit/survey_kit.dart';
 import 'package:woosignal/models/response/products.dart';
 import 'package:woosignal/models/response/tax_rate.dart';
 import 'package:woosignal/woosignal.dart';
+
+
+List<String> getRecommendations(SurveyResult r) {
+  List<String> s1ResultStrings = List.generate(1, (index) => "");
+  List<String> s2ResultStrings = List.generate(1, (index) => "");
+  List<List<String>> s2ResultStringsList = List.generate(1, (index) => s2ResultStrings);
+  List<StepResult> sResults = r.results;
+  for (int i = 0; i < sResults.length; i++) {
+    List<QuestionResult> qResults = sResults[i].results;
+    for (int j = 0; j < qResults.length; j++) {
+      if (j != 0) { break; }
+      else if (i == 0) { continue; }
+      else if (i < 7) {
+        String qResultString = qResults[j].valueIdentifier;
+        if (qResultString != null) {
+          List<String> qResultStrings = qResultString.split(',');
+          if (i == 1) { s1ResultStrings.addAll(qResultStrings); }
+          else { s2ResultStringsList.add(qResultStrings); }
+        }
+      }
+    }
+  }
+  List<String> recommendations = List.generate(1, (index) => ',');
+  for (int i = 0; i < s1ResultStrings.length; i++) {
+    String s1ResultString = s1ResultStrings[i];
+    for (int j = 0; j < s2ResultStringsList[i].length; j++) {
+      String s2ResultString = s2ResultStringsList[i][j];
+      switch (s1ResultString) {
+        case "tech":
+          switch (s2ResultString) {
+            case "systems": break;
+            case "software": break;
+            case "website": break;
+            case "other": break;
+          } break;
+        case "product":
+          switch (s2ResultString) {
+            case "usability": break;
+            case "appeal": break;
+            case "conversion": break;
+            case "other": break;
+          } break;
+        case "business":
+          switch (s2ResultString) {
+            case "performance": break;
+            case "continuity": break;
+            case "integration": break;
+            case "other": break;
+          } break;
+        case "graphics":
+          switch (s2ResultString) {
+            case "logo": break;
+            case "illustration": break;
+            case "collateral": break;
+            case "other": break;
+          } break;
+        case "content":
+          switch (s2ResultString) {
+            case "writing": break;
+            case "audio": break;
+            case "video": break;
+            case "other": break;
+          } break;
+        default:
+          break;
+      }
+    }
+  }
+  return recommendations;
+}
+
 
 Future<User> getUser() async =>
     (await NyStorage.read<User>(SharedKey.authUser, model: User()));
