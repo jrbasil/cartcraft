90f6b921549994a194176ab378a67e4ab8848e50
jrbasil <9315733+jrbasil@users.noreply.github.com>
2022-04-19 19:48:13 -0400

    Update survey styling

--- lib/resources/pages/survey/survey_page.dart
+++ lib/resources/pages/survey/survey_page.dart
@@ -1,179 +1,152 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
-import 'package:flutter_app/resources/pages/survey/result_page.dart';
+import 'package:flutter/material.dart';
 import 'package:nylo_framework/nylo_framework.dart';
 import 'package:survey_kit/survey_kit.dart';
 
 import '../../widgets/survey/custom_completion_step.dart';
+import '../survey/result_page.dart';
 
 class SurveyPage extends StatefulWidget {
-  // This widget is the survey page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   _SurveyPageState createState() => _SurveyPageState();
 }
 
-
 class _SurveyPageState extends State<SurveyPage> {
+  bool isLoading;
 
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    home: Scaffold(
+  void initState() {
+    super.initState();
+    isLoading = true;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
-        title: Text(
-          trans("Shopping Cart"),
-        ),
-        elevation: 1,
-        actions: <Widget>[
-          InkWell(
-            highlightColor: Colors.transparent,
-            splashColor: Colors.transparent,
-            child: Align(
-              child: Padding(
-                child: Text(
-                  trans("Clear Cart"),
-                  style: Theme.of(context).textTheme.bodyText2,
-                ),
-                padding: EdgeInsets.only(right: 8),
-              ),
-              alignment: Alignment.centerLeft,
-            ),
-          )
-        ],
         centerTitle: true,
+        title: Text(trans("Survey")),
       ),
-      body: Container(
-        color: Colors.black87,
-        child: Align(
-          alignment: Alignment.center,
-          child: FutureBuilder<Task>(
-            future: getSampleTask(),
-            builder: (context, snapshot) {
-              if (snapshot.connectionState == ConnectionState.done &&
-                  snapshot.hasData &&
-                  snapshot.data != null) {
-                final task = snapshot.data;
-                return SurveyKit(
-                  onResult: (SurveyResult r) {
-                    if (kDebugMode) {
-                      print(r.finishReason);
-                      setState(() {
-                        // This call to setState tells the Flutter framework that something has
-                        // changed in this State, which causes it to rerun the build method below
-                        // so that the display can reflect the updated values. If we changed
-                        // _counter without calling setState(), then the build method would not be
-                        // called again, and so nothing would appear to happen.
-                        Navigator.of(context).pop();
-                        // launch result page passing r argument
-                        // move/handle _generateRecommendations from result page
-                        Navigator.of(context).push(MaterialPageRoute(
-                            builder: (context) => ResultPage(
-                                title: "ResultPage", result: r)));
-                      });
-                    }
-                  },
-                  task: task,
-                  showProgress: true,
-                  localizations: const {
-                    'cancel': 'Cancel',
-                    'next': 'Next',
-                  },
-                  themeData: Theme.of(context).copyWith(
-                    outlinedButtonTheme: OutlinedButtonThemeData(
-                      style: ButtonStyle(
-                        minimumSize: MaterialStateProperty.all(
-                          const Size(150.0, 60.0),
-                        ),
-                        side: MaterialStateProperty.resolveWith(
-                              (Set<MaterialState> state) {
-                            if (state.contains(MaterialState.disabled)) {
-                              return const BorderSide(
-                                color: Colors.white,
-                              );
-                            }
+      body: SafeArea(
+        child: FutureBuilder<Task>(
+          future: getSampleTask(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done &&
+                snapshot.hasData &&
+                snapshot.data != null) {
+              final task = snapshot.data;
+              return SurveyKit(
+                onResult: (SurveyResult r) {
+                  if (kDebugMode) {
+                    print(r.finishReason);
+                    setState(() {
+                      // This call to setState tells the Flutter framework that something has
+                      // changed in this State, which causes it to rerun the build method below
+                      // so that the display can reflect the updated values. If we changed
+                      // _counter without calling setState(), then the build method would not be
+                      // called again, and so nothing would appear to happen.
+                      Navigator.of(context).pop();
+                      // launch result page passing r argument
+                      // move/handle _generateRecommendations from result page
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ResultPage(
+                              title: "ResultPage", result: r)));
+                    });
+                  }
+                },
+                task: task,
+                showProgress: true,
+                localizations: const {
+                  'cancel': 'Cancel',
+                  'next': 'Next',
+                },
+                themeData: Theme.of(context).copyWith(
+                  outlinedButtonTheme: OutlinedButtonThemeData(
+                    style: ButtonStyle(
+                      minimumSize: MaterialStateProperty.all(
+                        const Size(150.0, 60.0),
+                      ),
+                      side: MaterialStateProperty.resolveWith(
+                            (Set<MaterialState> state) {
+                          if (state.contains(MaterialState.disabled)) {
                             return const BorderSide(
-                              color: Colors.red,
+                              color: Colors.white,
                             );
-                          },
+                          }
+                          return const BorderSide(
+                            color: Colors.red,
+                          );
+                        },
+                      ),
+                      shape: MaterialStateProperty.all(
+                        RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(8.0),
                         ),
-                        shape: MaterialStateProperty.all(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(8.0),
-                          ),
-                        ),
-                        textStyle: MaterialStateProperty.resolveWith(
-                              (Set<MaterialState> state) {
-                            if (state.contains(MaterialState.disabled)) {
-                              return Theme.of(context)
-                                  .textTheme
-                                  .button
-                                  ?.copyWith(
-                                color: Colors.white,
-                              );
-                            }
+                      ),
+                      textStyle: MaterialStateProperty.resolveWith(
+                            (Set<MaterialState> state) {
+                          if (state.contains(MaterialState.disabled)) {
                             return Theme.of(context)
                                 .textTheme
                                 .button
                                 ?.copyWith(
-                              color: Colors.red,
+                              color: Colors.white,
                             );
-                          },
-                        ),
-                      ),
-                    ),
-                    textButtonTheme: TextButtonThemeData(
-                      style: ButtonStyle(
-                        textStyle: MaterialStateProperty.all(
-                          Theme.of(context).textTheme.button?.copyWith(
+                          }
+                          return Theme.of(context)
+                              .textTheme
+                              .button
+                              ?.copyWith(
                             color: Colors.red,
-                          ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  textButtonTheme: TextButtonThemeData(
+                    style: ButtonStyle(
+                      textStyle: MaterialStateProperty.all(
+                        Theme.of(context).textTheme.button?.copyWith(
+                          color: Colors.red,
                         ),
                       ),
                     ),
-                    textTheme: const TextTheme(
-                      headline2: TextStyle(
-                        fontSize: 21.0,
-                        color: Colors.red,
-                      ),
-                      headline5: TextStyle(
-                        fontSize: 18.0,
-                        color: Colors.red,
-                      ),
-                      bodyText2: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.black,
-                      ),
-                      subtitle1: TextStyle(
-                        fontSize: 15.0,
-                        color: Colors.black,
-                      ),
+                  ),
+                  textTheme: const TextTheme(
+                    headline2: TextStyle(
+                      fontSize: 21.0,
+                      color: Colors.red,
                     ),
-                    inputDecorationTheme: const InputDecorationTheme(
-                      labelStyle: TextStyle(
-                        color: Colors.black,
-                      ),
+                    headline5: TextStyle(
+                      fontSize: 18.0,
+                      color: Colors.red,
+                    ),
+                    bodyText2: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
+                    ),
+                    subtitle1: TextStyle(
+                      fontSize: 15.0,
+                      color: Colors.black,
                     ),
                   ),
-                  surveyProgressbarConfiguration: SurveyProgressConfiguration(
-                    backgroundColor: Colors.red,
+                  inputDecorationTheme: const InputDecorationTheme(
+                    labelStyle: TextStyle(
+                      color: Colors.black,
+                    ),
                   ),
-                );
-              }
-              return const CircularProgressIndicator.adaptive();
-            },
-          ),
+                ),
+                surveyProgressbarConfiguration: SurveyProgressConfiguration(
+                  backgroundColor: Colors.red,
+                ),
+              );
+            }
+            return const CircularProgressIndicator.adaptive();
+          },
         ),
       ),
-    ),
-  );
+    );
+  }
 
   static String formatText(String s) { return "What aspects of your " + s + " can be improved?"; }
   int currentSelectionIndex = 0; // Updated each time Q2 is completed for a Q1 input
@@ -409,6 +382,5 @@ class _SurveyPageState extends State<SurveyPage> {
     }
     return Future.value(task);
   }
+
 }
-
-
